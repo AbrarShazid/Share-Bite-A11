40f95b29e9a30222a21e147873cde190ba802c9c
Abrar Agmain Shazid <abraragmain75@gmail.com>
2025-06-11 02:33:01 +0600

    addfood page design complete

--- src/Layout/HomeLayOut.jsx
+++ src/Layout/HomeLayOut.jsx
@@ -5,15 +5,12 @@ import { Outlet } from 'react-router';
 
 const HomeLayOut = () => {
   return (
-    <div className='flex flex-col min-h-screen  '>
-      
-      <Navbar></Navbar>
-      <main className='flex-grow px-[3%]'>
-         <Outlet></Outlet>
+    <div className='min-h-screen flex flex-col'>
+      <Navbar />
+      <main className='flex-1 px-[3%] py-4'>
+        <Outlet />
       </main>
-     
-      <Footer></Footer>
-      
+      <Footer />
     </div>
   );
 };
